1f13eca47fccdc8c248c7cf3a8496a90f4e4744c
Olivia Waithira <oliviawaithira7@gmail.com>
2024-09-13 22:26:20 +0300

    json

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const emoji = [];
 
 async function getEmoji(){
   let response = await fetch("https://emoji-api.com/emojis?access_key=01b2ac974afedea89e2a3acc761de577efe5cb38")
+
+  data = await response.json()
+
+  console.log(data);
 }
 
 getEmoji()
