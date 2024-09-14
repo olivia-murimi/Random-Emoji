13f727b8791e5a9c0669ee8218991db3c051a339
Olivia Waithira <oliviawaithira7@gmail.com>
2024-09-14 21:34:57 +0300

    changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const btnEl document.getElementById("btn");
-const emojiNameEl document.getElementById("emoji-name");
+const btnEl = document.getElementById("btn");
+const emojiNameEl = document.getElementById("emoji-name");
 
 
 const emoji = [];
@@ -7,20 +7,21 @@ const emoji = [];
 async function getEmoji(){
   let response = await fetch("https://emoji-api.com/emojis?access_key=01b2ac974afedea89e2a3acc761de577efe5cb38")
 
-  data = await response.json()
+   let data = await response.json();
 
   for (let i=0; i<1500;i++){
     emoji.push({
        emojiName: data[i].character,
        emojiCode: data[i].unicodeName,
-    })
+    });
   }
 }
 
-getEmoji()
+getEmoji();
 
 btnEl.addEventListener("click", ()=>{
      const randomNum = Math.floor(Math.random() * emoji.length);
      console.log (randomNum);
      btnEl.innerText = emoji[randomNum].emojiName;
+     emojiNameEl.innerText = emoji[randomNum].emojiCode;
 })
